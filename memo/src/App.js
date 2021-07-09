311eeeb9d504f32d27a6e3c4726d88be9d8f4918
LeniKoehler <maria_m_koehler@hotmail.de>
2021-07-09 13:37:50 +0200

    feat(memo-card): created first memo card

---
+++ memo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ImgMediaCard from './components/Card.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImgMediaCard></ImgMediaCard>
+      </header>
+    </div>
+  );
+}
+
+export default App;
