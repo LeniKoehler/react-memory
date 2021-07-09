b02777322e4b8d26a75a2b8f27713c30ba8d3353
Amadeus Zittel <azittel@stud.hs-heilbronn.de>
2021-07-09 16:24:09 +0200

    feat(cardFlip): clicking on a card now changes its displayed image

--- memo/src/App.js
+++ memo/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import ImgMediaCard from './components/Card.js';
+import MemCard from './components/MemCard.js';
+import reactImageLogo from './img/logo-og.png';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <ImgMediaCard></ImgMediaCard>
+        <MemCard image={reactImageLogo}></MemCard>
       </header>
     </div>
   );
--- memo/src/components/MemCard.js
+++ memo/src/components/MemCard.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import image from '../img/logo-og.png';
+import backsideImage from '../img/backside.png'
 
 const useStyles = makeStyles({
   root: {
@@ -15,18 +11,28 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ImgMediaCard() {
+
+
+export default function MemCard(probs) {
+  const [revealed, setRevealed] = useState(false);
+  const [currentImage, setCurrentImage] = useState(backsideImage);
+  const [pairFound, setPairFound] = useState(false);
   const classes = useStyles();
+
+  function flipCard() {
+    setCurrentImage(revealed ? probs.image : backsideImage);
+    setRevealed(!revealed);
+  }
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
+      <CardActionArea disabled={pairFound} onClick={() => flipCard()}>
         <CardMedia
           component="img"
           alt="Contemplative Reptile"
           height="200"
           width="200"
-          image={image}
+          image={currentImage}
           title="React"
         />
       </CardActionArea>
